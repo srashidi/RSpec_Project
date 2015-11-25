d1b00bd9900974299834f2d667ca188c5436ae59
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-11-24 23:55:24 -0800

    RSpec tests for Cage class written and verified

--- connect_four/spec/cage_spec.rb
+++ connect_four/spec/cage_spec.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
 
-RED = "\u{1F534}"
-BLACK = "\u{2b24}"
-
 describe Cage do
 
-	before :all do
+	before :each do
 		@cage = Cage.new
 	end
 
@@ -36,14 +33,100 @@ describe Cage do
 
 	describe "#place_piece" do
 
-		before :each do
-			@cage.place_piece(:red,3)
-		end
-
 		context "when column is empty" do
+
+			before :each do
+				@cage.place_piece(:red,3)
+			end
+
 			it "puts piece on the bottom row in the correct column" do
+				expect(@cage.row1).to eql "|    |    |    |    |    |    |    |"
+				expect(@cage.row2).to eql "|    |    |    |    |    |    |    |"
+				expect(@cage.row3).to eql "|    |    |    |    |    |    |    |"
+				expect(@cage.row4).to eql "|    |    |    |    |    |    |    |"
+				expect(@cage.row5).to eql "|    |    |    |    |    |    |    |"
 				expect(@cage.row6).to eql "|    |    | #{RED}  |    |    |    |    |"
 			end
+
+			it "puts piece on the bottom row in a different column" do
+				@cage.place_piece(:black,5)
+				expect(@cage.row1).to eql "|    |    |    |    |    |    |    |"
+				expect(@cage.row2).to eql "|    |    |    |    |    |    |    |"
+				expect(@cage.row3).to eql "|    |    |    |    |    |    |    |"
+				expect(@cage.row4).to eql "|    |    |    |    |    |    |    |"
+				expect(@cage.row5).to eql "|    |    |    |    |    |    |    |"
+				expect(@cage.row6).to eql "|    |    | #{RED}  |    | #{BLACK}  |    |    |"
+			end
+
+		end
+
+		context "when column has at least one piece" do
+
+			before :each do
+				@cage.place_piece(:red,3)
+				@cage.place_piece(:black,5)
+				@cage.place_piece(:red,3)
+			end
+
+			it "puts piece on the row above the bottom row in the correct column" do
+				expect(@cage.row1).to eql "|    |    |    |    |    |    |    |"
+				expect(@cage.row2).to eql "|    |    |    |    |    |    |    |"
+				expect(@cage.row3).to eql "|    |    |    |    |    |    |    |"
+				expect(@cage.row4).to eql "|    |    |    |    |    |    |    |"
+				expect(@cage.row5).to eql "|    |    | #{RED}  |    |    |    |    |"
+				expect(@cage.row6).to eql "|    |    | #{RED}  |    | #{BLACK}  |    |    |"
+			end
+
+			context "when another piece is placed in the same column" do
+				it "puts piece on the row above that row in the correct column" do
+					@cage.place_piece(:black,3)
+					expect(@cage.row1).to eql "|    |    |    |    |    |    |    |"
+					expect(@cage.row2).to eql "|    |    |    |    |    |    |    |"
+					expect(@cage.row3).to eql "|    |    |    |    |    |    |    |"
+					expect(@cage.row4).to eql "|    |    | #{BLACK}  |    |    |    |    |"
+					expect(@cage.row5).to eql "|    |    | #{RED}  |    |    |    |    |"
+					expect(@cage.row6).to eql "|    |    | #{RED}  |    | #{BLACK}  |    |    |"
+				end
+			end
+
+		end
+
+		context "when column is filled" do
+
+			before do
+				@cage.place_piece(:red,3)
+				@cage.place_piece(:black,3)
+				@cage.place_piece(:red,3)
+				@cage.place_piece(:black,3)
+				@cage.place_piece(:red,3)
+				@cage.place_piece(:black,3)
+			end
+
+			context "when a piece is placed in the same column" do
+				it "gives an error message" do
+					expect(STDOUT).to receive(:puts).with("Error: Column is full. Try again...")
+					@cage.place_piece(:red,3)
+					expect(@cage.row1).to eql "|    |    | #{BLACK}  |    |    |    |    |"
+					expect(@cage.row2).to eql "|    |    | #{RED}  |    |    |    |    |"
+					expect(@cage.row3).to eql "|    |    | #{BLACK}  |    |    |    |    |"
+					expect(@cage.row4).to eql "|    |    | #{RED}  |    |    |    |    |"
+					expect(@cage.row5).to eql "|    |    | #{BLACK}  |    |    |    |    |"
+					expect(@cage.row6).to eql "|    |    | #{RED}  |    |    |    |    |"
+				end
+			end
+
+			context "when a piece is placed in a different, open column" do
+				it "places the piece in the correct spot" do
+					@cage.place_piece(:red,5)
+					expect(@cage.row1).to eql "|    |    | #{BLACK}  |    |    |    |    |"
+					expect(@cage.row2).to eql "|    |    | #{RED}  |    |    |    |    |"
+					expect(@cage.row3).to eql "|    |    | #{BLACK}  |    |    |    |    |"
+					expect(@cage.row4).to eql "|    |    | #{RED}  |    |    |    |    |"
+					expect(@cage.row5).to eql "|    |    | #{BLACK}  |    |    |    |    |"
+					expect(@cage.row6).to eql "|    |    | #{RED}  |    | #{RED}  |    |    |"
+				end
+			end
+
 		end
 
 	end
--- connect_four/spec/spec_helper.rb
+++ connect_four/spec/spec_helper.rb
@@ -2,6 +2,9 @@ require_relative '../connect_four'
 require_relative '../player'
 require_relative '../cage'
 
+RED = "\u{1F534}"
+BLACK = "\u{2b24}"
+
 class Cage
 
 	attr_reader :row1, :row2, :row3, :row4, :row5, :row6
