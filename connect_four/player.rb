a629b1162298cda1ff58e8ca5c1d37e5ee9f0a61
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-11-27 14:18:28 -0800

    Documentation for ConnectFour generated

--- connect_four/player.rb
+++ connect_four/player.rb
@@ -2,9 +2,12 @@ class Player
 
 	# Player object's color is readable
 	attr_reader :color
+
+  #Player object's pieces array is accessible
 	attr_accessor :pieces
 
-	# Create Player object with given color
+	# Create Player object with given color and
+  # a blank pieces array
 	def initialize(color)
 		@color = color
 		@pieces = []
